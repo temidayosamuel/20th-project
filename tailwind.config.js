2e29b1ceca26d6ffded34d66e81357bd6291d669
TemidayoSamuel <temidayosamue@gmail.com>
2023-09-16 11:06:00 -0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        VeryDarkBlue: "hsl(243, 87%, 12%)",
+        Pink: "hsl(322, 100%, 66%)",
+        LightPink: "hsl(321, 100%, 78%)",
+        LightRed: "hsl(0, 100%, 63%)",
+        VeryDarkCyan: "hsl(192, 100%, 9%)",
+        VeryPaleBlue: "hsl(207, 100%, 98%)",
+        LightGrayishBlue: "hsl(240, 75%, 98%)",
+        LightGray: "hsl(0, 0%, 75%)",
+      },
+    },
+  },
+  plugins: [],
+};
